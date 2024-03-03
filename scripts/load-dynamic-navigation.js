b31b817fe60ccc400d59f0308570b92689f3e158
Ole Lammers <ole.lammers@pm.me>
2024-03-02 18:13:12 -0800

    feat: coverted header to dynamic element

---
+++ scripts/load-dynamic-navigation.js
@@ -0,0 +1,33 @@
+async function getSkeleton() {
+	const html = fetch('/skeletons/navigation.xml');
+
+	const el = document.createElement('object');
+	el.type = 'text/xml';
+	el.data = './skeletons/navigation.xml';
+
+	return el;
+}
+
+function fillDynamicsInNavigation(el) {
+	const path = window.location.pathname;
+
+	const titleEl = el.querySelector('[data-fillwith=navbar-title]');
+	if (titleEl) titleEl.innerText = document.title;
+
+	const activeA = el.querySelector(`a[href="${location.pathname}"].nav-link`);
+	console.log(location.pathname, activeA);
+	if (activeA) activeA.classList.add('active');
+}
+
+(async () => {
+	const container = document.getElementById('navigation-placeholder');
+	if (!container) return console.warn('Dynamic navigation script loaded, but no placeholder found.');
+
+	await fetch('/skeletons/navigation.xml')
+		.then((v) => v.text())
+		.then((v) => {
+			const next = container.nextElementSibling;
+			container.outerHTML = v;
+			fillDynamicsInNavigation(next.previousElementSibling);
+		});
+})();
