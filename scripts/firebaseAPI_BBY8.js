7fbd1aeabd2a39b9b84d86c02f527a37a2a72a0f
Ole Lammers <ole.lammers@pm.me>
2024-03-06 11:04:08 -0800

    feat: added firebase to lists page

--- scripts/firebaseAPI_BBY8.js
+++ scripts/firebaseAPI_BBY8.js
@@ -3,12 +3,12 @@
 //----------------------------------------
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD0y7_d13aSCeJbc6M0OYTaJJVod7msc7A",
-  authDomain: "comp1800-bby8.firebaseapp.com",
-  projectId: "comp1800-bby8",
-  storageBucket: "comp1800-bby8.appspot.com",
-  messagingSenderId: "284571994979",
-  appId: "1:284571994979:web:34d9dc37ed42b079162156"
+	apiKey: 'AIzaSyD0y7_d13aSCeJbc6M0OYTaJJVod7msc7A',
+	authDomain: 'comp1800-bby8.firebaseapp.com',
+	projectId: 'comp1800-bby8',
+	storageBucket: 'comp1800-bby8.appspot.com',
+	messagingSenderId: '284571994979',
+	appId: '1:284571994979:web:34d9dc37ed42b079162156',
 };
 
 //--------------------------------------------
@@ -18,3 +18,16 @@ const firebaseConfig = {
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const storage = firebase.storage();
+
+function logout() {
+	firebase
+		.auth()
+		.signOut()
+		.then(() => {
+			// Sign-out successful.
+			console.log('logging out user');
+		})
+		.catch((error) => {
+			// An error happened.
+		});
+}
