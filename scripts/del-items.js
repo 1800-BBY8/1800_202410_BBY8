43a80d75be8b473486aa2c328ec9bcbc928a28d4
Hugo Amuan <hugo.amuan@icloud.com>
2024-03-18 10:48:25 -0700

    tiny fixes

--- scripts/del-items.js
+++ scripts/del-items.js
@@ -1,4 +1,4 @@
-// Reference to Firebase database
+
 var database = firebase.database().ref('items');
 
 // Function to fetch and display item data in the form for editing
@@ -13,15 +13,12 @@ function populateEditForm(itemId) {
         editItemForm.querySelector('#itemName').value = itemData.item_name;
         editItemForm.querySelector('#category').value = itemData.category;
         editItemForm.querySelector('#description').value = itemData.description;
-        // You'll need to handle image population based on how you store images in Firebase
-        // For example, if you store the image URL in the database:
-        // editItemForm.querySelector('#itemImage').src = itemData.image_url;
+        editItemForm.querySelector('#itemImage').src = itemData.image_url;
 
-        // Update the form action URL to include itemId for updating the correct item
+       
         editItemForm.action = '/update_item?id=' + itemId;
     });
 }
 
-// Call the function with the itemId when you want to populate the edit form
 var itemId = 'your_item_id_here'; // Replace 'your_item_id_here' with the actual itemId
 populateEditForm(itemId);
