63b64f5aec5a27f95e19e4203ac9527baa7630e3
Zyrakia <ole.lammers@pm.me>
2024-03-21 13:14:39 -0700

    refactor: changed item pricing dialogue to focus on units

--- scripts/trip.js
+++ scripts/trip.js
@@ -3,28 +3,22 @@ import { getList } from './firestore-utils/list-helpers.js';
 const params = new URLSearchParams(location.search);
 const id = params.get('id');
 
-const priceFormTemplate = document.getElementById('price-form-template');
 const tripNameHolder = document.getElementById('trip-name');
+const priceFormTemplate = document.getElementById('price-form-template');
+const itemTemplate = document.getElementById('item-template');
+const addItemButton = document.getElementById('add-item-button');
 
 const trip = {};
+
+function renderItem(itemData) {}
 
 async function promptForPrice() {
 	const frag = priceFormTemplate.content.cloneNode(true);
 
-	let useUnit = false;
 	const itemsBoughtInput = frag.querySelector('.template-items-bought-input');
-	const itemPriceInput = frag.querySelector('.template-item-price-input');
-	const unitCheck = frag.querySelector('.template-specify-unit-check');
-	const unitFieldset = frag.querySelector('.template-unit-fieldset');
-	const unitPriceInput = frag.querySelector('.template-unit-price-input');
+	const eachPriceInput = frag.querySelector('.template-each-price-input');
 	const unitAmountInput = frag.querySelector('.template-unit-amount-input');
-	const unitSelect = frag.querySelector('.template-unit-select');
-
-	unitCheck.addEventListener('change', (e) => {
-		const checked = e.target.checked;
-		unitFieldset.disabled = !checked;
-		useUnit = checked;
-	});
+	const unitTypeSelect = frag.querySelector('.template-unit-select');
 
 	const refId = '_______promptFormContainer';
 	const container = document.createElement('div');
@@ -39,20 +33,12 @@ async function promptForPrice() {
 		},
 		preConfirm: () => {
 			const quantity = parseInt(itemsBoughtInput.value);
-			const eachPrice = parseFloat(itemPriceInput.value);
-			if (isNaN(quantity) || isNaN(eachPrice)) return;
-
-			const res = { quantity, eachPrice };
-			if (!useUnit) return res;
-
-			const unitPrice = parseFloat(unitPriceInput.value);
+			const boughtAtPrice = parseFloat(eachPriceInput.value);
 			const unitAmount = parseFloat(unitAmountInput.value);
-			const unit = unitSelect.value;
-			if (isNaN(unitPrice) || isNaN(unitAmount)) return res;
+			const unitType = unitTypeSelect.value;
 
-			res.unit = { unitPrice, unitAmount, unit };
-
-			return res;
+			if (isNaN(quantity) || isNaN(boughtAtPrice) || isNaN(unitAmount)) return;
+			return { quantity, boughtAtPrice, boughtAtUnit: { unit: unitType, amount: unitAmount } };
 		},
 		showConfirmButton: true,
 		showDenyButton: true,
@@ -65,12 +51,14 @@ async function promptForPrice() {
 
 async function setupTripFromList(list) {
 	tripNameHolder.innerText = `${list.name} Trip`;
+	for (const item of list.items) renderItem(item);
 }
 
 async function setupAnonTrip() {
 	tripNameHolder.innerText = 'New Quick Trip';
 	// TODO test
 	const priceDefinition = await promptForPrice();
+	console.log(priceDefinition);
 }
 
 async function setupTrip(id) {
