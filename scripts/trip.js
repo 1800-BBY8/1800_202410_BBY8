d525179591cd4c996d49f9537a59c78d16b4e430
Zyrakia <ole.lammers@pm.me>
2024-03-22 13:15:55 -0700

    feat: marking items as done and price calculation

--- scripts/trip.js
+++ scripts/trip.js
@@ -1,31 +1,81 @@
-import { getList } from './firestore-utils/list-helpers.js';
+import { getListWithResolvedItems } from './firestore-utils/list-helpers.js';
 
 const params = new URLSearchParams(location.search);
 const id = params.get('id');
 
 const tripNameHolder = document.getElementById('trip-name');
+const tripPriceHolder = document.getElementById('trip-price');
 const priceFormTemplate = document.getElementById('price-form-template');
 const itemTemplate = document.getElementById('item-template');
 const addItemButton = document.getElementById('add-item-button');
 const itemsContainer = document.getElementById('items-container');
 
-const trip = {};
+const priceFormatter = Intl.NumberFormat('en-US', { style: 'currency', currency: 'CAD' });
 
-function renderItem(itemData) {
+const trip = {
+	boughtItems: [],
+};
+
+let totalTaxRate = 0.12;
+async function cacheTaxRate() {
+	if (totalTaxRate) return;
+
+	const bcTaxes = await fetch('https://api.salestaxapi.ca/v2/province/bc').then((v) => v.json());
+	const rateTypes = bcTaxes.type.split(',');
+	rateTypes.forEach((v) => (totalTaxRate += bcTaxes[v]));
+}
+
+function calculateTotal() {
+	const subtotal = trip.boughtItems.reduce((acc, v) => {
+		if (!v.boughtAtPrice) return acc;
+		return acc + v.boughtAtPrice * v.quantity;
+	}, 0);
+
+	return subtotal + subtotal * totalTaxRate;
+}
+
+function updateTrip() {
+	const total = calculateTotal();
+	tripPriceHolder.innerText = priceFormatter.format(total);
+}
+
+function renderItem(listItem) {
+	const { quantity: _quantity, item } = listItem;
+	const quantity = parseInt(_quantity);
 	const frag = itemTemplate.content.cloneNode(true);
 
 	const name = frag.querySelector('.template-name');
-	name.innerText = itemData.itemName;
+	name.innerText = item.itemName;
 
 	const qty = frag.querySelector('.template-quantity');
-	qty.innerText = itemData.quantity;
+	qty.innerText = `${quantity}/${quantity}`;
+	if (!quantity) qty.remove();
 
-	// TODO checkmark
+	const check = frag.querySelector('.template-select');
+	check.addEventListener('change', async (e) => {
+		const checked = e.target.checked;
+		e.target.disabled = true;
+
+		if (checked) {
+			const priceDefinition = await promptForPrice(quantity);
+			trip.boughtItems.push({ item, ...priceDefinition });
+			check.indeterminate = priceDefinition.quantity < quantity;
+
+			qty.innerText = `${quantity - priceDefinition.quantity}/${quantity}`;
+		} else {
+			const index = trip.boughtItems.findIndex((v) => v.item === item);
+			trip.boughtItems.splice(index, 1);
+			qty.innerText = `${quantity}/${quantity}`;
+		}
+
+		e.target.disabled = false;
+		updateTrip();
+	});
 
 	itemsContainer.appendChild(frag);
 }
 
-async function promptForPrice() {
+async function promptForPrice(initQuantity = 1) {
 	const frag = priceFormTemplate.content.cloneNode(true);
 
 	const itemsBoughtInput = frag.querySelector('.template-items-bought-input');
@@ -36,6 +86,8 @@ async function promptForPrice() {
 	const refId = '_______promptFormContainer';
 	const container = document.createElement('div');
 	container.id = refId;
+
+	itemsBoughtInput.value = initQuantity;
 
 	const res = await Swal.fire({
 		title: 'Price Definition',
@@ -50,12 +102,12 @@ async function promptForPrice() {
 			const unitAmount = parseFloat(unitAmountInput.value);
 			const unitType = unitTypeSelect.value;
 
-			if (isNaN(quantity) || isNaN(boughtAtPrice) || isNaN(unitAmount)) return;
+			if (isNaN(quantity) || isNaN(boughtAtPrice) || isNaN(unitAmount)) return { quantity };
 			return { quantity, boughtAtPrice, boughtAtUnit: { unit: unitType, amount: unitAmount } };
 		},
 		showConfirmButton: true,
 		showDenyButton: true,
-		denyButtonText: 'Ignore Price',
+		denyButtonText: 'Do Not Specify',
 		confirmButtonText: 'Confirm',
 	});
 
@@ -69,17 +121,16 @@ async function setupTripFromList(list) {
 
 async function setupAnonTrip() {
 	tripNameHolder.innerText = 'New Quick Trip';
-	// TODO test
-	const priceDefinition = await promptForPrice();
-	console.log(priceDefinition);
 }
 
 async function setupTrip(id) {
 	if (!id) return setupAnonTrip();
 
-	const list = await getList(id);
+	const list = await getListWithResolvedItems(id);
 	if (list) setupTripFromList(list);
 	else setupAnonTrip();
 }
 
-setupTrip(id);
+// TODO allow for add custom item, and saved item
+
+cacheTaxRate().then(() => setupTrip(id));
