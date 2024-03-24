6466c7fdcde621f2c0d630002f66d3269a537a8b
Tanner Parkes <Tanner.lp9@gmail.com>
2024-03-24 14:21:04 -0700

    Added: 	- Button functionality for categories and overall spending. 	- Added placeholder image. 	- Made clicking one button would hide the other graphs added and placeholder.

--- scripts/insights.js
+++ scripts/insights.js
@@ -1,37 +1,100 @@
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
 
-function drawChart() {
-  var data = new google.visualization.DataTable();
 
-  // Todo
-  // Placeholder as default upon page navigation
-  // When selecting item, need a form to select which item from a list.
-  // Categories will simply display all different categories in a pie chart with percentages based on total cost for whichever timeframe selected.
-  // Overall spending will display trips and their costs on a bar chart. Total cost displayed.   
+// Event listener for the categories pie chart.
+document.getElementById('categories').addEventListener('click', function() {
+  // Hide placeholder image and other charts
+  document.getElementById('placeholderImage').style.display = 'none';
+  //document.getElementById('chart2_div').style.display = 'none';
+  //document.getElementById('chart3_div').style.display = 'none';
+  // Show chart container
+  //document.getElementById('chart1_div').style.display = 'block';
 
-  data.addColumn('string', 'Month');
-  data.addColumn('number', 'Butter');
+  const chart1 = document.getElementById('chart1_div');
+  chart1.classList.toggle("d-none", false); //shows the div for categories
+  document.getElementById('chart2_div').classList.toggle("d-none", true);
+  document.getElementById('chart3_div').classList.toggle("d-none", true);
 
-  // Data set for graph.
-  // Add connection to database.
-  // Left will be dates and right will be the price data
-  data.addRows([
-    ['January', 4.64],
-    ['February', 4.99],
-    ['March', 5.15],
-    ['April', 5.45],
-    ['May', 5.99]
-  ]);
+  // Load Google Charts
+  google.charts.load('current', {'packages':['corechart']});
+  google.charts.setOnLoadCallback(drawChart);
 
-  var options = {
-    title: 'Item Costs',
-    curveType: 'function',
-    legend: { position: 'bottom' },
-    backgroundColor: 'transparent',
-  };
+  function drawChart() {
 
-  var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+    var data = new google.visualization.DataTable();
 
-  chart.draw(data, options);
-}
+    data.addColumn('string', 'Category');
+    data.addColumn('number', 'Totals');
+
+    data.addRows([
+      ['Protein', 700],
+      ['Carbohydrates', 200],
+      ['Produce', 100],
+      ['Fats', 150],
+      ['Snacks', 300],
+      ['Drinks', 400],
+      ['Misc', 1000]
+    ]);
+
+    var options = {
+      title: 'Category Spending',
+      curveType: 'function',
+      legend: { position: 'bottom' },
+      backgroundColor: 'transparent',
+    };
+
+    var chart = new google.visualization.PieChart(chart1);
+
+    $(window).resize(function(){
+      chart.draw(data, options);
+    });
+
+    chart.draw(data, options);
+  }
+});
+
+// Event listener for the overall total. 
+document.getElementById('overall').addEventListener('click', function() {
+  // Hide placeholder image and other charts
+  document.getElementById('placeholderImage').style.display = 'none';
+  //document.getElementById('chart2_div').style.display = 'none';
+  //document.getElementById('chart3_div').style.display = 'none';
+  // Show chart container
+  //document.getElementById('chart1_div').style.display = 'block';
+
+  const chart2 = document.getElementById('chart2_div');
+  chart2.classList.toggle("d-none", false); //shows the div for categories
+  document.getElementById('chart1_div').classList.toggle("d-none", true);
+  document.getElementById('chart3_div').classList.toggle("d-none", true);
+
+  // Load Google Charts
+  google.charts.load('current', {'packages':['corechart']});
+  google.charts.setOnLoadCallback(drawChart);
+
+  function drawChart() {
+
+    var data = new google.visualization.DataTable();
+
+    data.addColumn('string', 'Category');
+    data.addColumn('number', 'Totals');
+
+    data.addRows([
+      ['Total', 2850]
+    ]);
+
+    var options = {
+      title: 'Overall Spending',
+      curveType: 'function',
+      legend: { position: 'bottom' },
+      backgroundColor: 'transparent',
+      pieSliceText: 'value'
+    };
+
+    var chart = new google.visualization.PieChart(chart2);
+
+    $(window).resize(function(){
+      chart.draw(data, options);
+    });
+
+    chart.draw(data, options);
+  }
+});
